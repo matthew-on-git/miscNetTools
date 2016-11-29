6f7fe2562e954f68ee15e8aef09bb45599e10330
Matthew Mellor <matthew.j.mellor@gmail.com>
2016-11-29 11:19:40 -0800

    made changes to make multiple hosts work

--- pullRunningConf.py
+++ pullRunningConf.py
@@ -2,28 +2,31 @@ import napalm
 
 # Instructions
 print ''
-print 'This script will ask for a working directory, hostname, and credentials for a device, or list of devices seperated by a ",". It will then attempt to connect to those devices and copy their running config to the directory you specified named by "hostname.config"'
+print 'This script will ask for a working directory, hostname, and credentials for a device, or list of devices seperated by a space. It will then attempt to connect to those devices and copy their running config to the directory you specified named by "hostname.config"'
 print ''
 # Get the detes 
-print 'What directory would you like to save the config backups to? (e.g. /home/netops/switch_baks)'
+print 'What directory would you like to save the config backups to? (e.g. "/home/netops/switch_baks")'
 working_dir = raw_input()
-print 'What nos is your device running? (e.g. nxos, ios, panos, etc.)'
+print 'What nos is your device running? (e.g. "nxos", "ios", "panos", etc.)'
 nos = raw_input()
-print 'What devices would you like to save the config backups for? (e.g. device-01.corp.com,device-02.corp.com'
-devices = raw_input()
-print 'What username should I use to connect to the devices? (e.g. admin)'
+print 'What devices would you like to save the config backups for? (e.g. "device-01.corp.com device-02.corp.com"'
+hosts_input = raw_input()
+hosts = hosts_input.split()
+print 'What username should I use to connect to the devices? (e.g. "admin")'
 username = raw_input()
-print 'What password should I use to connect to the devices? (e.g. password)'
+print 'What password should I use to connect to the devices? (e.g. "password")'
 passwd = raw_input()
 # Confirm input
 print 'The working directory is (%s)' % (working_dir)
-print 'The devices to backup are (%s)' % (devices)
+print 'The devices to backup are (%s)' % (hosts)
 print 'Confirm and continue? (yes or no)'
 confirm = raw_input().lower()
 if confirm is 'y' or 'yes':
     # do the thing
     print 'Fetching running configs ...'
     driver = get_network_driver(nos)
+    for host in hosts:
+        driver(hosts, username, passwd)
+        device.open()
 else:
-    # cancel
     print 'operation cancelled by user'
