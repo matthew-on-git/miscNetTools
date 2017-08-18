0bb95172c46d209ebcc6c979982e797e0871bfd6
Matthew Mellor <matthew.j.mellor@gmail.com>
2017-08-18 14:30:32 -0700

    added elastalert install script (psuedocode untested first draft)

---
+++ installElastalert.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -x
+## Install dependencies
+yum install -y python-setuptools python-devel gcc foo
+easy_install pip
+easy_install -U setuptools
+pip install setuptools --upgrade
+## Install Elastalert
+pip install elastalert
+## Depending on the version of Elasticsearch, you may need to 
+## manually install the correct version of elasticsearch-py.
+## Elasticsearch 5.0+:
+# pip install "elasticsearch>=5.0.0"
+## Elasticsearch 2.X:
+# pip install "elasticsearch<3.0.0"
+mkdir /opt/elastalert
+mkdir /opt/elastalert/rules.d
